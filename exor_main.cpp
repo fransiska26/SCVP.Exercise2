cf75d4fda7e2c024358f114edb8c80977e571367
Fransiska <fransiska26@hotmail.com>
2024-02-28 12:20:21 +0100

    waveform tracing and clocked process (task 3 and 4)

--- exor_main.cpp
+++ exor_main.cpp
@@ -4,13 +4,23 @@
 #include "exor.h"
 #include "mon.h"
 
+
 int sc_main(int, char**)
 {
     sc_signal<bool> sigA, sigB, sigZ;
 
+    // Setup Waveform Tracing:
+    sc_trace_file *wf = sc_create_vcd_trace_file("traceFile");
+    sc_trace(wf, sigA, "sigA");
+    sc_trace(wf, sigB, "sigB");
+    sc_trace(wf, sigZ, "sigZ");
+
+    sc_clock clock("clk", sc_time(20, SC_NS));
+
     stim Stim1("Stimulus");
     Stim1.A(sigA);
     Stim1.B(sigB);
+    Stim1.Clk(clock);
 
     exor DUT("exor");
     DUT.A(sigA);
@@ -21,8 +31,12 @@ int sc_main(int, char**)
     mon.A(sigA);
     mon.B(sigB);
     mon.Z(sigZ);
+    mon.Clk(clock);
 
     sc_start();  // run forever
 
+        // Close Trace File:
+    sc_close_vcd_trace_file(wf);
+    
     return 0;
 }
--- stim.h
+++ stim.h
@@ -6,11 +6,13 @@
 SC_MODULE(stim)
 {
 public:
+    sc_in<bool> Clk; 
     sc_out<bool> A, B;
 
     SC_CTOR(stim)
     {
         SC_THREAD(StimGen);
+        sensitive << Clk;
     }
 
 private:
@@ -19,19 +21,24 @@ private:
         wait(SC_ZERO_TIME);
         A.write(false);
         B.write(false);
-        wait(10, SC_NS);
+        wait();
+        //wait(10, SC_NS);
         A.write(false);
         B.write(true);
-        wait(15, SC_NS);
+        wait();
+        //wait(15, SC_NS);
         A.write(true);
         B.write(false);
-        wait(10, SC_NS);
+        wait();
+        //wait(10, SC_NS);
         A.write(true);
         B.write(true);
-        wait(10, SC_NS);
+        wait();
+        //wait(10, SC_NS);
         A.write(false);
         B.write(false);
-        wait(10, SC_NS);
+        wait();
+        //wait(10, SC_NS);
         sc_stop();
     }
 };
